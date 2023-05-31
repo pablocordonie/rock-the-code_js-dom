e27990c183cfa609cd92f744faede7bdb16d2fa0
pablocordonie <pablogarcia.93.06@gmail.com>
2023-05-31 19:43:17 +0200

    set the filters menu next to the header in styles.css & delete the class asigned to the aside in aside.js

--- components/Aside/aside.js
+++ components/Aside/aside.js
@@ -1,6 +1,6 @@
 export const filtersAside = () => {
     const aside = document.createElement('aside');
-    aside.classList.add('rtc--swingman-filters-menu');
+
     const imgWrapper = document.createElement('div');
     imgWrapper.classList.add('rtc--swingman-filters-menu-imgContainer');
 
